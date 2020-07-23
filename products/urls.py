2808698cefd90852a7f1baf350a196961d719e1b
Poulami96 <duttapoulami96@gmail.com>
2020-07-23 13:50:09 +0530

    first commit

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from .views import ProductDetailView,ProductListView
+from .views import product_list,product_detail,manufacturer_list,manufacturer_detail
 
 urlpatterns = [
-    path("",ProductListView.as_view(),name="product-list"),
-    path("products/<int:pk>/",ProductDetailView.as_view(),name="product-detail"),
+    path("products/",product_list,name="product-list"),
+    path("products/<int:pk>/",product_detail,name="product-detail"),
+    path("manufacturer/",manufacturer_list,name="manufacturer-list"),
+    path("manufacturer/<int:pk>/",manufacturer_detail,name="manufacturer-detail"),
     
 ]
